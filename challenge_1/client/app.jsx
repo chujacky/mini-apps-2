07489fa01035369bce366882f3b16019ab266b1b
chujacky <jacky9407@gmail.com>
2019-01-29 19:45:44 -0800

    Render simple app from bundle

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('body'));
 
