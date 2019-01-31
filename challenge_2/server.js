f6cadc02f4991fdf0be00bd0d2dbef9336923508
chujacky <jacky9407@gmail.com>
2019-01-31 10:41:04 -0800

    Setup development environment and render hello world

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
+const app = express();
 
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Bitcoin tracker listening to PORT ${PORT}`);
+})
