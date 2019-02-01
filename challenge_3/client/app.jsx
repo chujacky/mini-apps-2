0d101ab0e50756f9d10b2673ff1d089e3de1a5b3
chujacky <jacky9407@gmail.com>
2019-02-01 15:21:13 -0800

    Build and render simple scorecard

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PinButtons from './components/PinButtons.jsx';
+import Scorecard from './components/Scorecard.jsx';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-
-    }
+      frame: 1,
+      scorecard: [
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0]},
+        {frameScore: 0, bowlScores: [0, 0, 0]},
+      ],
+    };
   }
 
   render() {
+    const { frame, scorecard } = this.state;
     return (
       <div>
         <h2>Bowling Scorecard</h2>
         <PinButtons />
+        <Scorecard 
+          frame={frame}
+          scorecard={scorecard}
+        />
       </div>
     );
   }
---
+++ challenge_3/client/components/ScorecardFrame.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ScorecardFrame = ({ frameScore, frame }) => {
+  return (
+    <div className="frame-box">
+      <div className="frame-label">{frame}</div>
+      <div className="bowls-box">
+        {frameScore.bowlScores.map((bowl, index) => {
+          return (
+            <div key={`bowl${index}`} >{bowl}</div>
+          );
+        })}
+      </div>
+      <div className="frame-total">{frameScore.frameScore}</div>
+    </div>
+  )
+
+}
+
+export default ScorecardFrame;
