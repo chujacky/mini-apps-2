8dff5f017260824d1f6994301db20dea3b55c5ae
chujacky <jacky9407@gmail.com>
2019-02-01 12:16:08 -0800

    Setup environment for app

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
 
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('main'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,16 @@
+const express = require ('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3772;
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Bowling scorecard listening to PORT ${PORT}`);
+})
 
 
